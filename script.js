961f4918b75a9355077e73645dd541109108cd95
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-04-29 14:05:57 +0300

    add knowledge about DOM
    know how to select, create, insert and delete DOM element
    
    One of the most useful lectures,
    will be using this as an reference for the future.

--- script.js
+++ script.js
@@ -22,7 +22,6 @@ const closeModal = function () {
 
 btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
-
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
@@ -31,3 +30,47 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons =  document.getElementsByTagName("button");
+console.log(allButtons);
+
+console.log(document.getElementsByClassName("btn"));
+
+// Creating and inserting elements
+
+const message =  document.createElement("div");
+message.classList.add("cookie-message");
+// message.textContent = "We use cookies for improved functionallity and analytics.";
+message.innerHTML = 'We use cookies for improved functionallity and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// Delete elements
+document.querySelector(".btn--close-cookie").addEventListener("click", function (){
+  message.remove();
+  // message.parentElement.removeChild(message)
+});
+
+
+
+
+
