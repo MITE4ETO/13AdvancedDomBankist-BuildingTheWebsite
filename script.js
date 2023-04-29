8bb9ef57171102757e77724234eac49c4169bc56
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-04-29 15:29:50 +0300

    add styles, attributes and classes
    getComputedStyle()
    getAttribute()
    
    // Classes
    logo.classList.add("c", "j")
    logo.classList.remove("c", "j")
    logo.classList.toggle("c")
    logo.classList.contains("c") // not includes

--- script.js
+++ script.js
@@ -33,7 +33,6 @@ document.addEventListener('keydown', function (e) {
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
 // Selecting elements
 console.log(document.documentElement);
 console.log(document.head);
@@ -45,17 +44,18 @@ const allSections = document.querySelectorAll('.section');
 console.log(allSections);
 
 document.getElementById('section--1');
-const allButtons =  document.getElementsByTagName("button");
+const allButtons = document.getElementsByTagName('button');
 console.log(allButtons);
 
-console.log(document.getElementsByClassName("btn"));
+console.log(document.getElementsByClassName('btn'));
 
 // Creating and inserting elements
 
-const message =  document.createElement("div");
-message.classList.add("cookie-message");
+const message = document.createElement('div');
+message.classList.add('cookie-message');
 // message.textContent = "We use cookies for improved functionallity and analytics.";
-message.innerHTML = 'We use cookies for improved functionallity and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+message.innerHTML =
+  'We use cookies for improved functionallity and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 
 // header.prepend(message);
 header.append(message);
@@ -65,12 +65,55 @@ header.append(message);
 // header.after(message);
 
 // Delete elements
-document.querySelector(".btn--close-cookie").addEventListener("click", function (){
-  message.remove();
-  // message.parentElement.removeChild(message)
-});
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+    // message.parentElement.removeChild(message)
+  });
 
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
 
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
 
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
 
+document.documentElement.style.setProperty('--color-primary', 'orangered');
 
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'Beatufil minimalist logo';
+
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute("company", "Bankist");
+
+console.log(logo.src);
+console.log(logo.getAttribute("src"));
+
+const link = document.querySelector(".nav__link--btn");
+console.log(link.href);
+console.log(link.getAttribute("href"));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add("c", "j")
+logo.classList.remove("c", "j")
+logo.classList.toggle("c")
+logo.classList.contains("c") // not includes
+
+// Dont use
+logo.className = "jonas"
