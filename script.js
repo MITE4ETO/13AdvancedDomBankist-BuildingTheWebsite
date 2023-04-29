ab90f5a8f0a07334bd5e05c1e485c1496c93f5f9
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-04-29 16:23:30 +0300

    takeout from this lecture section1.scrollIntoView({     behavior: 'smooth',   });
    section1.scrollIntoView({
        behavior: 'smooth',
      });

--- script.js
+++ script.js
@@ -33,6 +33,7 @@ document.addEventListener('keydown', function (e) {
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/*
 // Selecting elements
 console.log(document.documentElement);
 console.log(document.head);
@@ -117,3 +118,38 @@ logo.classList.contains("c") // not includes
 
 // Dont use
 logo.className = "jonas"
+*/
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'height/widht viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top : s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({
+    behavior: 'smooth',
+  });
+});
