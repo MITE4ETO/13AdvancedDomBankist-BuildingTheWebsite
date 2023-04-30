1a62c7eac6ad389cd05518d3fb9cbff25de00aee
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-04-30 13:44:10 +0300

    add knowledge DOM TRAVERSING and event delegation

--- script.js
+++ script.js
@@ -83,7 +83,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
   // Matching strategy
   if (e.target.classList.contains('nav__link')) {
-    
     const id = e.target.getAttribute('href');
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
@@ -258,3 +257,32 @@ document.querySelector('.nav').addEventListener(
   }
 )
 */
+
+const h1 = document.querySelector('h1');
+
+// Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'white';
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children); // getting all the children elements
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
