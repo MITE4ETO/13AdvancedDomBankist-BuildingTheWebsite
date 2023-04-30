295fe5b551937c0176792a6a8d1e73a6c38b00ad
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-04-30 11:55:03 +0300

    understood more about bubbling and capturing

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ logo.classList.contains("c") // not includes
 
 // Dont use
 logo.className = "jonas"
-*/
+
 
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
@@ -148,7 +148,7 @@ btnScrollTo.addEventListener('click', function (e) {
   //   top : s1coords.top + window.pageYOffset,
   //   behavior: 'smooth',
   // });
-
+/*
   section1.scrollIntoView({
     behavior: 'smooth',
   });
@@ -169,3 +169,33 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 // h1.onmouseenter = function (e){
 //   alert("onmouseenter: Great! You are reading the heading :D")
 // };
+*/
+
+// rgb(255,255,255)
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // Stop event propagation
+  // e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener(
+  'click',
+  function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log('NAV', e.target, e.currentTarget);
+  }
+);
