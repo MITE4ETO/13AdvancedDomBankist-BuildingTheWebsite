2388e32554c38193a3043ca1b86d6aa28419aca1
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-05-01 11:31:26 +0300

    add revealing elements on scroll
    added nice feature using IntersectionObserver()
    
    basically the opacity of the elements are set to 0, but when we scroll down, we reveal the elements by animation, it looks pretty nice and it is frequently used in the sites

--- script.js
+++ script.js
@@ -162,8 +162,6 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 
 const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
-console.log(navHeight);
-
 
 const stickyNav = function (entries) {
   const [entry] = entries;
@@ -180,6 +178,28 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 
 headerObserver.observe(header);
+
+// Reveal sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
