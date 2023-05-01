394dcb22dc173e804f59b1e30cb092fbf5bd93a1
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-05-01 21:16:32 +0300

    useful DOM lifestyle events

--- script.js
+++ script.js
@@ -515,3 +515,17 @@ console.log(h1.parentElement.children); // getting all the children elements
   if (el !== h1) el.style.transform = 'scale(0.5)';
 });
 */
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree built!', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded', e);
+});
+
+// window.addEventListener('beforeunLoad', function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = "";
+// });
